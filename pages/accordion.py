ebded0965e663dc27dcbcbe5c4d0ebd4b6ecbbe1
MariaBeng <mashaskl@mail.ru>
2024-06-19 20:26:55 +0300

    4

--- pages/accordion.py
+++ pages/accordion.py
@@ -1,6 +1,6 @@
 from pages.base_page import BasePage
 from components.components import WebElement
-class accordian(BasePage):
+class Accordion(BasePage):
 
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/accordian'
--- tests/tests_hw/Test_visible_hw.py
+++ tests/tests_hw/Test_visible_hw.py
@@ -1,18 +1,18 @@
 import time
-from pages.accordian import accordian
+from pages.accordion import Accordion
 def test_visible_accordion(browser):
-    acc = accordian(browser)
-    acc.visit()
-    assert acc.elem.visible()
-    acc.elem1.click()
+    accordion = Accordion(browser)
+    accordion.visit()
+    assert accordion.elem.visible()
+    accordion.elem1.click()
     time.sleep(2)
 
-    assert not acc.elem.visible()
+    assert not accordion.elem.visible()
 
 
 def test_visible_accordian_default(browser):
-    acc = accordian(browser)
-    acc.visit()
-    assert not acc.elem2.visible()
-    assert not acc.elem3.visible()
-    assert not acc.elem4.visible()
+    accordion = Accordion(browser)
+    accordion.visit()
+    assert not accordion.elem2.visible()
+    assert not accordion.elem3.visible()
+    assert not accordion.elem4.visible()
