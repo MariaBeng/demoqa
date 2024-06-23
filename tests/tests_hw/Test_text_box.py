c74473d46d75c60ce57798d741a238abcd1f562b
MariaBeng <mashaskl@mail.ru>
2024-06-23 17:05:59 +0300

    7

--- tests/tests_hw/Test_text_box.py
+++ tests/tests_hw/Test_text_box.py
@@ -1,13 +1,16 @@
 from pages.text_box import TextBox
 import time
 
+Name = 'Cherry'
+Address = 'Saint P'
+
 def test_box(browser):
     text_box = TextBox(browser)
 
     text_box.visit()
-    text_box.full_name.send_keys('Cherry')
-    text_box.current_address.send_keys('Saint P')
+    text_box.full_name.send_keys(Name)
+    text_box.current_address.send_keys(Address)
 
     text_box.btn_submit.click_force()
     time.sleep(5)
-    assert text_box.checking.get_text() == 'Name:Cherry\nCurrent Address :Saint P'
+    assert text_box.checking.get_text() == f'Name:{Name}\nCurrent Address :{Address}'
