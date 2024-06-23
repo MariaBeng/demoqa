4ade96d9a7fc10da8be04deec9e4202c2f972c2b
MariaBeng <mashaskl@mail.ru>
2024-06-23 16:49:47 +0300

    5

---
+++ tests/tests_hw/Test_login_form_validate.py
@@ -0,0 +1,13 @@
+from pages.practice_form import PracticeForm
+
+
+def test_placeholder(browser):
+    practice_form = PracticeForm(browser)
+
+    practice_form.visit()
+    # assert practice_form.first_name.get_dom_attribute('placeholder') == 'First Name'
+    # assert practice_form.last_name.get_dom_attribute('placeholder') == 'Last Name'
+    # assert practice_form.user_email.get_dom_attribute('placeholder') == 'name@example.com'
+
+    practice_form.btn_submit.click_force()
+    assert practice_form.form.get_dom_attribute('class') == 'was-validated'
